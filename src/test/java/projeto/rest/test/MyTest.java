b79db2eec3e1a303ad57cf2269a55e4b78aece51
tamirismv <tamiris.mv07@gmail.com>
2024-02-28 21:40:21 -0300

    revisão de teste

--- src/test/java/projeto/rest/test/MyTest.java
+++ src/test/java/projeto/rest/test/MyTest.java
@@ -1,8 +1,8 @@
 package projeto.rest.test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.Assert.assertEquals;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
@@ -14,7 +14,7 @@ public class MyTest {
 	@Test
 	public void validarLista() {
 		response = RestAssured.get("https://reqres.in/api/users?page=2");
-		assertEquals(200, response.statusCode());
+		assertEquals(response.statusCode(),200);
 
 	}
 
